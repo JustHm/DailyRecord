274800336e617184f9474662148bbf5b4dbae818
JustHm <taggma12@gmail.com>
2023-05-22 21:37:57 +0900

    OpenAPI 서비스 추가

--- DailyRecord/Service/WeatherService.swift
+++ DailyRecord/Service/WeatherService.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct WeatherService {
+    private func getCurrentLocationWeather() async throws -> Data {
+        let url = URL(string: "www.weather.com")!
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode == 200 else { throw CocoaError(.fileLocking) }
+        
+        return data
+    }
+}
